959ba4e35ff72127e7195d1256fc647f178870d0
Fash <artemav2006@mail.ru>
2024-06-20 01:10:06 +1000

    +Важное изменение

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let money = 0;
 
 document.getElementById('clickButton').addEventListener('click', function() {
   money += 1;
-  document.getElementById('moneyDisplay').innerText = 'Money: ' + money;
+  document.getElementById('moneyDisplay').innerText = 'Бабки: ' + money;
 });
